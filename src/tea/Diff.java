aa6b6824fd67a83d29633d7f77df7c85d63ce8e0
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2019-10-27 11:05:39 -0400

    added comparator for list
    also cleanup

--- src/tea/Diff.java
+++ src/tea/Diff.java
@@ -14,6 +14,39 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+interface Differencer<V> {
+    Stream<Difference> differences(V left, V right);
+}
+
+interface DifferencerForKey<K, V> {
+    Differencer<V> forKey(K key);
+}
+
+class Difference {
+    private final String msg;
+
+    public Difference(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString() {
+        return msg;
+    }
+
+    public static <K, T> Difference rightOnly(K key, T right) {
+        return new Difference("" + key + " = " + right.toString() + " in right only!");
+    }
+
+    public static <K, T> Difference leftOnly(K key, T left) {
+        return new Difference("" + key + " = " + left.toString() + " in left only!");
+    }
+
+    public static <T, K> Differencer<T> unequalKey(K key) {
+        return (T left, T right) -> Stream.of(new Difference("" + key + ": " + left.toString() + " != " + right.toString()));
+    }
+}
+
 public class Diff {
 
     public static void main(String[] args) {
@@ -42,19 +75,24 @@ public class Diff {
     }
 
     private static Stream<Difference> compareList(List<Person> left, List<Person> right) {
-        return compareMap(listToMap(left), listToMap(right));
-
+        return compareList(left, right, Diff.ifTestKey(Person::equals, Difference::unequalKey));
     }
 
-    private static Stream<Difference> compareMap(Map<Integer, Person> left, Map<Integer, Person> right) {
-        var commonKeys = left.keySet().stream().filter(right::containsKey).collect(Collectors.toSet());
-        var leftOnlyKeys = left.keySet().stream().filter(Predicate.not(commonKeys::contains));
-        var rightOnlyKeys = right.keySet().stream().filter(Predicate.not(commonKeys::contains));
-        return Stream.concat(
-                diffInCommon(commonKeys, left, right, ifTestKey(Person::equals, Difference::unequalKey)),
-                Stream.concat(
-                        diffLeftOnly(leftOnlyKeys, left::get),
-                        diffRightOnly(rightOnlyKeys, right::get)));
+    private static <V> Stream<Difference> compareList(List<V> left, List<V> right, DifferencerForKey<Integer, V> differencer) {
+        return compareMap(differencer).differences(listToMap(left), listToMap(right));
+    }
+
+    private static <K, V> Differencer<Map<K, V>> compareMap(DifferencerForKey<K, V> differencer) {
+        return (Map<K, V> left, Map<K, V> right) -> {
+            var commonKeys = left.keySet().stream().filter(right::containsKey).collect(Collectors.toSet());
+            var leftOnlyKeys = left.keySet().stream().filter(Predicate.not(commonKeys::contains));
+            var rightOnlyKeys = right.keySet().stream().filter(Predicate.not(commonKeys::contains));
+            return Stream.concat(
+                    diffInCommon(commonKeys, left, right, differencer),
+                    Stream.concat(
+                            diffLeftOnly(leftOnlyKeys, left::get),
+                            diffRightOnly(rightOnlyKeys, right::get)));
+        };
     }
 
     private static <K, V> Stream<Difference> diffRightOnly(Stream<K> keys, Function<K, V> right) {
@@ -65,57 +103,26 @@ public class Diff {
         return keys.map(k -> Difference.leftOnly(k, left.apply(k)));
     }
 
-    private static <K, V> KeyDifferencer<K, V> ifTestKey(BiPredicate<V, V> equals, KeyDifferencer<K, V> differencer) {
+    private static <K, V> DifferencerForKey<K, V> ifTestKey(BiPredicate<V, V> equals, DifferencerForKey<K, V> differencer) {
         return k -> ifTest(equals, differencer.forKey(k));
     }
 
     private static <V> Differencer<V> ifTest(BiPredicate<V, V> equals, Differencer<V> differencer) {
-        return (l, r) -> !equals.test(l, r) ? Stream.empty() : differencer.differences(l, r);
-    }
-
-    interface Differencer<V> {
-        Stream<Difference> differences(V left, V right);
-    }
-
-    interface KeyDifferencer<K, V> {
-        Differencer<V> forKey(K key);
+        return (l, r) -> equals.test(l, r) ? Stream.empty() : differencer.differences(l, r);
     }
 
     private static <K, V> Stream<Difference> diffInCommon(
             Set<K> keys,
             Map<K, V> left,
             Map<K, V> right,
-            KeyDifferencer<K, V> compareValues) {
+            DifferencerForKey<K, V> compareValues) {
         return keys.stream()
                 .flatMap(k -> compareValues.forKey(k).differences(left.get(k), right.get(k)));
     }
 
-    private static Map<Integer, Person> listToMap(List<Person> list) {
+    private static <V> Map<Integer, V> listToMap(List<V> list) {
         return IntStream.range(0, list.size()).boxed().collect(Collectors.toMap(Function.identity(), list::get));
     }
 
-    private static class Difference {
-        private final String msg;
 
-        public Difference(String msg) {
-            this.msg = msg;
-        }
-
-        @Override
-        public String toString() {
-            return msg;
-        }
-
-        public static <K, T> Difference rightOnly(K key, T right) {
-            return new Difference("" + key + " = " + right.toString() + " in right only!");
-        }
-
-        public static <K, T> Difference leftOnly(K key, T left) {
-            return new Difference("" + key + " = " + left.toString() + " in left only!");
-        }
-
-        public static <T, K> Differencer<T> unequalKey(K key) {
-            return (T left, T right) -> Stream.of(new Difference("" + key + ": " + left.toString() + " != " + right.toString()));
-        }
-    }
 }
